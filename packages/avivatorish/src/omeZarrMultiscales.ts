9343b03762ced106fe44c613defbd3a814948cc3
Peter Todd <ptr.tdd@gmail.com>
2026-03-26 16:38:25 +0000

    remove redundant omeZarrMultiscales.ts

--- packages/avivatorish/src/omeZarrMultiscales.ts
+++
@@ -1,7 +0,0 @@
-import { loadOmeZarr } from '@hms-dbmi/viv';
-
-/** OME-Zarr multiscales pixel sources for a URL (no caching). */
-export async function loadOmeZarrMultiscalesData(url: string): Promise<unknown> {
-  const loader = await loadOmeZarr(url, { type: 'multiscales' });
-  return loader.data;
-}
